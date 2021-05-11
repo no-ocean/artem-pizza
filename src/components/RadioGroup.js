255b90583b4eb922782b9906f811e358afa409f4
Borisenko Anton <aborisenko@xbsoftware.by>
2021-05-11 13:50:37 +0300

    feat: radiogroup component for unite radiobuttons

--- src/components/RadioGroup.js
+++ src/components/RadioGroup.js
@@ -1,17 +1,17 @@
 import React from "react";
 import RadioButton from "./RadioButton";
 
-const RadioGroup = ({label, data}) => {
-
+const RadioGroup = ({title, data, order, onChange}) => {
+   
     const radioItems = data.map((item) => {
         let id = Math.floor(Math.random() * 1000000);
-        return <RadioButton key={id} itemConfig={item} />
+        return <RadioButton key={id} itemConfig={item} order={order}/>
     });
 
     return (
         <div className="col flex flex-col">
-            <span className="mb-10">{label}</span>
-            <div className="flex radiogroup">
+            <span className="mb-10">{title}</span>
+            <div className="flex radiogroup" onChange={onChange}>
                 { radioItems }
             </div>
         </div>
