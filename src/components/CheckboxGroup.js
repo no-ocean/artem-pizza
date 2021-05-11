b55fc573d1bfb845894d7e677708d910f13c77bf
Borisenko Anton <aborisenko@xbsoftware.by>
2021-05-11 14:10:48 +0300

    feat: dev checkbox group component

--- src/components/CheckboxGroup.js
+++ src/components/CheckboxGroup.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Checkbox from "./Checkbox";
 
-const CheckboxGroup = ({label, data}) => {
+const CheckboxGroup = ({title, data, order, onChange}) => {
 
     const checkboxItems = data.map((item) => {
         let id = Math.floor(Math.random() * 1000000);
-        return <Checkbox key={id} itemConfig={item} />
+        return <Checkbox key={id} itemConfig={item} order={order}/>
     });
 
     return (
         <div className="col flex flex-col">
-            <span className="mb-10">{label}</span>
-            <div className="flex space-between checkboxGroup">
+            <span className="mb-10">{title}</span>
+            <div className="flex space-between checkboxGroup" onChange={onChange}>
                 { checkboxItems }
             </div>
         </div>
