d4d2eba698bc1968301de74371af9a909afbb85b
Borisenko Anton <aborisenko@xbsoftware.by>
2021-05-11 13:49:09 +0300

    feat: radio button component with params

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-const RadioButton = ({itemConfig}) => {
+const RadioButton = ({itemConfig, order}) => {
     
-    const { name, val, label, isChecked=false } = itemConfig;
+    const { name, val, label, price } = itemConfig; 
 
     return (
         <div className="radio mr-10">
             <label className="flex flex-v-center">
                 <span>{label}</span>
-                <input type="radio" name={name} value={val} defaultChecked={isChecked} />
+                <input
+                    type="radio" 
+                    name={name} 
+                    value={val}
+                    defaultChecked={order[name] === val}
+                    data-price={price}
+                />
             </label>
         </div>
     );
