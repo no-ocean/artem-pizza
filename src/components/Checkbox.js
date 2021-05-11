5c69e61f5ebd62c5526224a38778c26b900b6e2e
Borisenko Anton <aborisenko@xbsoftware.by>
2021-05-11 14:06:04 +0300

    feat: dev checkbox component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Checkbox = ({itemConfig}) => {
+const Checkbox = ({itemConfig, order}) => {
     
-    const { name, val, label, price, isChecked=false } = itemConfig;
+    const { name, val, label, price } = itemConfig;
 
     return (
         <div className="checkbox mr-10">
@@ -10,7 +10,13 @@ const Checkbox = ({itemConfig}) => {
                 <span>{label}</span>
                 <div className="flex flex-v-center space-between">
                     <span className="mr-10">{price} &#8381;</span>
-                    <input type="checkbox" name={name} value={val} defaultChecked={isChecked} />
+                    <input 
+                        type="checkbox" 
+                        name={name} 
+                        value={val} 
+                        defaultChecked={order[name].includes(val)}
+                        data-price={price}
+                    />
                 </div>
             </label>
         </div>
