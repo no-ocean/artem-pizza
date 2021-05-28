722d48056b2d4b756d69855858fd9ed74a15cc2c
Borisenko Anton <aborisenko@xbsoftware.by>
2021-05-28 17:13:50 +0300

    refactor: add ids to data items

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ const dataConfig = {
 		title: "Размер",
 		data: [
 			{
+				id: 1,
 				name: "size",
 				val: "30 см",
 				label: "30 см",
 				price: 0
 			},
 			{
+				id: 2,
 				name: "size",
 				val: "35 см",
 				label: "35 см",
@@ -23,12 +25,14 @@ const dataConfig = {
 		title: "Тесто",
 		data: [
 			{
+				id: 1,
 				name: "dough",
 				val: "Тонкое",
 				label: "Тонкое",
 				price: 0
 			},
 			{
+				id: 2,
 				name: "dough",
 				val: "Пышное",
 				label: "Пышное",
@@ -40,24 +44,28 @@ const dataConfig = {
 		title: "Выберите соус",
 		data: [
 			{
+				id: 1,
 				name: "sauces",
 				val: "Томатный",
 				label: "Томатный",
 				price: 0
 			},
 			{
+				id: 2,
 				name: "sauces",
 				val: "Майонезный",
 				label: "Майонез",
 				price: 0
 			},
 			{
+				id: 3,
 				name: "sauces",
 				val: "Острый",
 				label: "Острый",
 				price: 0
 			},
 			{
+				id: 4,
 				name: "sauces",
 				val: "Грибной",
 				label: "Грибной",
@@ -69,18 +77,21 @@ const dataConfig = {
 		title: "Добавьте сыр",
 		data: [
 			{
+				id: 1,
 				name: "cheese",
 				val: "Моцарелла",
 				label: "Моцарелла",
-				price: 29,
+				price: 29
 			},
 			{
+				id: 2,
 				name: "cheese",
 				val: "Чеддер",
 				label: "Чеддер",
 				price: 29
 			},
 			{
+				id: 3,
 				name: "cheese",
 				val: "Дор Блю",
 				label: "Дор Блю",
@@ -92,18 +103,21 @@ const dataConfig = {
 		title: "Добавьте овощи",
 		data: [
 			{
+				id: 1,
 				name: "vegetables",
 				val: "Помидоры",
 				label: "Помидоры",
 				price: 29
 			},
 			{
+				id: 2,
 				name: "vegetables",
 				val: "Грибы",
 				label: "Грибы",
 				price: 29
 			},
 			{
+				id: 3,
 				name: "vegetables",
 				val: "Перец",
 				label: "Перец",
@@ -115,18 +129,21 @@ const dataConfig = {
 		title: "Добавьте мясо",
 		data: [
 			{
+				id: 1,
 				name: "meat",
 				val: "Бекон",
 				label: "Бекон",
 				price: 29
 			},
 			{
+				id: 2,
 				name: "meat",
 				val: "Пепперони",
 				label: "Пепперони",
 				price: 29
 			},
 			{
+				id: 3,
 				name: "meat",
 				val: "Ветчина",
 				label: "Ветчина",
