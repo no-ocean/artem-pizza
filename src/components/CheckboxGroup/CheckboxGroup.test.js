595ade51edf5f6753241bf7594b3fb464a321ae2
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-06 00:46:39 +0300

    test: default rendering checkbox group

--- src/components/CheckboxGroup/CheckboxGroup.test.js
+++ src/components/CheckboxGroup/CheckboxGroup.test.js
@@ -1,37 +1,36 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import CheckboxGroup from "./CheckboxGroup";
-import { useForm } from "react-hook-form";
 
-const { register, handleSubmit } = useForm();
+const testFn = jest.fn();
 
-const data = {
-    mozarella: { id: 1, name: "cheese", val: "Моцарелла", label: "Моцарелла", price: 29 },
-    cheddar: { id: 2, name: "cheese", val: "Чеддер", label: "Чеддер", price: 29 },
-    dorblu: { id: 3, name: "cheese", val: "Дор Блю", label: "Дор Блю", price: 29 }
-};
+const data = [
+    { "id": 1, "category": "cheese", "name": "Моцарелла", "slug": "mozarella", "price": "100" },
+    { "id": 2, "category": "cheese", "name": "Чеддер", "slug": "cheddar", "price": "100" },
+    { "id": 3, "category": "cheese", "name": "Дор Блю", "slug": "dor-blue", "price": "100" }
+];
 
 describe("default rendering checkbox group", () => {
     it("should render radiogroup component", () => {   
-        render(<CheckboxGroup title="Добавьте сыр" data={data} register={register}/>);
+        render(<CheckboxGroup title="Добавьте сыр" data={data} register={testFn}/>);
         const checkboxGroup = screen.getByText(/Добавьте сыр/i);
         expect(checkboxGroup).toBeInTheDocument();
     });
     
-    it("should be default checked radiobutton", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={register}/>);
-        const checkboxItem = screen.getByDisplayValue(/Моцарелла/i);
-        expect(checkboxItem).toBeChecked();
+    it("mozarella should not be checked", () => {
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        const checkboxItem = screen.getByDisplayValue(/mozarella/i);
+        expect(checkboxItem).not.toBeChecked();
     });
     
-    it("should not be default checked radiobutton", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={register}/>);
-        const checkboxItem = screen.getByDisplayValue(/Чеддер/i);
+    it("cheddar should not be checked", () => {
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        const checkboxItem = screen.getByDisplayValue(/cheddar/i);
         expect(checkboxItem).not.toBeChecked();
     });
 
-    it("should not be default checked radiobutton", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={register}/>);
-        const checkboxItem = screen.getByDisplayValue(/Дор Блю/i);
+    it("dor blue should not be checked", () => {
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        const checkboxItem = screen.getByDisplayValue(/dor-blue/i);
         expect(checkboxItem).not.toBeChecked();
     });
 });
