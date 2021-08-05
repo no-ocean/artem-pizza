d710e226d0c1fccaa9693736b986b65886fae34e
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-06 00:53:24 +0300

    test: default rendering radio group and check click events

--- src/components/RadioGroup/RadioGroup.test.js
+++ src/components/RadioGroup/RadioGroup.test.js
@@ -1,29 +1,29 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import RadioGroup from "./RadioGroup";
 
-const data = [
-    { id: 1, name: "size", val: "30 см", label: "30 см", price: 0 },
-    { id: 2, name: "size", val: "35 см", label: "35 см", price: 50 }
-];
+const testFn = jest.fn();
 
-const order = { size: "30 см" };
+const data = [
+    { "id": 1, "category": "size", "name": "30 см", "slug": "30", "price": "0" },
+    { "id": 2, "category": "size", "name": "35 см", "slug": "35", "price": "50" }
+];
 
 describe("default rendering radiogroup", () => {
     it("should render radiogroup component", () => {   
-        render(<RadioGroup title="Размер" data={data} order={order}/>);
+        render(<RadioGroup title="Размер" data={data} register={testFn}/>);
         const radioGroup = screen.getByText(/размер/i);
         expect(radioGroup).toBeInTheDocument();
     });
     
-    it("should be default checked radiobutton", () => {
-        render(<RadioGroup title="Размер" data={data} order={order}/>);
-        const radioButton = screen.getByDisplayValue(/30 см/i);
-        expect(radioButton).toBeChecked();
+    it("should not be default checked 30 radiobutton", () => {
+        render(<RadioGroup title="Размер" data={data} register={testFn}/>);
+        const radioButton = screen.getByDisplayValue(/30/i);
+        expect(radioButton).not.toBeChecked();
     });
     
-    it("should not be default checked radiobutton", () => {
-        render(<RadioGroup title="Размер" data={data} order={order}/>);
-        const radioButton = screen.getByDisplayValue(/35 см/i);
+    it("should not be default checked 35 radiobutton", () => {
+        render(<RadioGroup title="Размер" data={data} register={testFn}/>);
+        const radioButton = screen.getByDisplayValue(/35/i);
         expect(radioButton).not.toBeChecked();
     });
     
@@ -33,8 +33,8 @@ describe("events", () => {
     it("should toggle radiobuttons", () => {
         const handleRadio = jest.fn();
     
-        render(<RadioGroup title="Размер" data={data} order={order} onChange={handleRadio}/>);
-        const radioButton = screen.getByDisplayValue(/35 см/i);
+        render(<RadioGroup title="Размер" data={data} register={testFn} onChange={handleRadio}/>);
+        const radioButton = screen.getByDisplayValue(/35/i);
         expect(radioButton).not.toBeChecked();
     
         fireEvent.click(radioButton);
