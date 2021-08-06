b938589f1dc1045bb6a885ac5684bdc74a236a42
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-06 14:10:36 +0300

    test: checkbox group tests for render and events

--- src/components/CheckboxGroup/CheckboxGroup.test.js
+++ src/components/CheckboxGroup/CheckboxGroup.test.js
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import CheckboxGroup from "./CheckboxGroup";
 
-const testFn = jest.fn();
+const mockRegister = jest.fn();
 
 const data = [
     { "id": 1, "category": "cheese", "name": "Моцарелла", "slug": "mozarella", "price": "100" },
@@ -11,26 +11,41 @@ const data = [
 
 describe("default rendering checkbox group", () => {
     it("should render radiogroup component", () => {   
-        render(<CheckboxGroup title="Добавьте сыр" data={data} register={testFn}/>);
+        render(<CheckboxGroup title="Добавьте сыр" data={data} register={mockRegister}/>);
         const checkboxGroup = screen.getByText(/Добавьте сыр/i);
         expect(checkboxGroup).toBeInTheDocument();
     });
     
     it("mozarella should not be checked", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={mockRegister}/>);
         const checkboxItem = screen.getByDisplayValue(/mozarella/i);
         expect(checkboxItem).not.toBeChecked();
     });
     
     it("cheddar should not be checked", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={mockRegister}/>);
         const checkboxItem = screen.getByDisplayValue(/cheddar/i);
         expect(checkboxItem).not.toBeChecked();
     });
 
     it("dor blue should not be checked", () => {
-        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={testFn}/>);
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={mockRegister}/>);
         const checkboxItem = screen.getByDisplayValue(/dor-blue/i);
         expect(checkboxItem).not.toBeChecked();
     });
 });
+
+describe("checkbox events", () => {
+    it("should check checkboxes", () => {
+        render(<CheckboxGroup  title="Добавьте сыр" data={data} register={mockRegister}/>);
+
+        const checkbox = screen.getByDisplayValue(/mozarella/i);
+        expect(checkbox).not.toBeChecked();
+
+        fireEvent.click(checkbox);
+        expect(checkbox).toBeChecked();
+
+        fireEvent.click(checkbox);
+        expect(checkbox).not.toBeChecked();
+    })
+})
