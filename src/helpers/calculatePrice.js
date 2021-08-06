0c25b42ca5e6d5cf24a0803a68a2294f2c7779b5
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-06 15:33:53 +0300

    refactor: calculate final price function

--- src/helpers/calculatePrice.js
+++ src/helpers/calculatePrice.js
@@ -1,32 +1,16 @@
 export const calculatePrice = (data, values, defaultPrice) => {
 
-    const { size, dough, sauces, cheese, vegetables, meat } = values;
+    const { size, cheese, vegetables, meat } = values;
 
-    // const meatPrice = meat.reduce((acc, item) => {
-    //     const instance = data.find((dataItem) => dataItem.slug === item);
-    //     return acc + +instance.price;
-    // }, 0); 
+    const calcCheckboxes = (value) => {
+        return value.reduce((acc, item) => {
+            return acc + +data[item].price;
+        }, 0);
+    };
 
-    const cheesePrice = cheese.reduce((acc, item) => {
-        return acc + +data[item].price;
-    }, 0);
+    const calcRadio = (value) => {
+        return +data[value].price;
+    }
 
-    const meatPrice = meat.reduce((acc, item) => {
-        return acc + +data[item].price;
-    }, 0);
-
-    const vegetablesPrice = vegetables.reduce((acc, item) => {
-        return acc + +data[item].price;
-    }, 0);
-
-
-    // const sizePrice = SIZE.data[size].price;
-    // const doughPrice = DOUGH.data[dough].price;
-    // const saucesPrice = SAUCES.data[sauces].price;
-
-    // const cheesePrice = cheese.reduce((price, cheese) => {
-    //     return price + CHEESE.data[cheese].price;
-    // }, 0);
-
-    return defaultPrice + cheesePrice + vegetablesPrice + meatPrice;
-}
+    return defaultPrice + calcRadio(size) + calcCheckboxes(cheese) + calcCheckboxes(vegetables) + calcCheckboxes(meat);
+};
