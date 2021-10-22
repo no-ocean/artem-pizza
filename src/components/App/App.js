6ae802a5d9189fb0bfd302c855b9d8d1368fcf1e
Borisenko Anton <aborisenko@xbsoftware.by>
2021-10-22 14:26:53 +0300

    dev: header component with styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../Header";
 import Configurator from "../Configurator";
 import Registration from "../Registration";
 import Login from "../Login";
 import Order from "../Order";
+import OrderList from "../OrderList";
 import PageNotFound from "../PageNotFound";
 import { Route, Link, Switch } from "react-router-dom";
 import OrderSuccess from "../OrderSuccess/OrderSuccess";
@@ -10,32 +12,33 @@ import OrderSuccess from "../OrderSuccess/OrderSuccess";
 const App = () => {
 	return (
 		<div className="App">
-			<nav className="mb-30">
-				<Link className="mr-10" to="/">App</Link>
-				<Link className="mr-10" to="/registration">Registration</Link>
-				<Link className="mr-10" to="/login">Login</Link>
-			</nav>
-			<h1>Собери свою пиццу</h1>
-			<Switch>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/registration">
-					<Registration />
-				</Route>
-				<Route path="/order">
-					<Order/>
-				</Route>
-				<Route path="/success">
-					<OrderSuccess />
-				</Route>
-				<Route exact path="/">
-					<Configurator />
-				</Route>
-				<Route>
-					<PageNotFound />
-				</Route>
-			</Switch>
+			<Header />
+			<div className="container">
+				<h1>Собери свою пиццу</h1>
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/registration">
+						<Registration />
+					</Route>
+					<Route path="/orders">
+						<OrderList />
+					</Route>
+					<Route path="/order">
+						<Order />
+					</Route>
+					<Route path="/success">
+						<OrderSuccess />
+					</Route>
+					<Route exact path="/">
+						<Configurator />
+					</Route>
+					<Route>
+						<PageNotFound />
+					</Route>
+				</Switch>
+			</div>
 		</div>
 	);
 }
