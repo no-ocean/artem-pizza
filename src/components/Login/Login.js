a2edc27d30db05c42df9dd95276c2baf0a84695b
Borisenko Anton <aborisenko@xbsoftware.by>
2021-11-19 20:21:24 +0300

    dev: form styles and error schema

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { setLogin } from "../../state/login/actions";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+	email: yup.string().email('Неправильный формат').required("Введите email"),
+	password: yup.string().required("Введите пароль")
+});
 
 const Login = () => {
 	const dispatch = useDispatch();
-	const { register, handleSubmit } = useForm();
+	const { register, watch, handleSubmit, formState: {errors} } = useForm({
+		defaultValues: {email: "", password: ""},
+		resolver: yupResolver(schema)
+	});
 
 	const onSubmit = (data) => {
 		dispatch(setLogin(true));
 		alert(JSON.stringify(data))
 	};
+
+	let emailClass = `input ${errors.email ? "input_error" : ""}`;
+	let passClass = `input ${errors.password ? "input_error" : ""}`;
+
+	console.log("errors")
+	console.log(errors)
+
+	let values = watch();
+    let btnFlag = (values.email.length > 0 || values.password.length) > 0 ? false : true;
 
 	return (
 		<div className="screen-light">
@@ -25,23 +44,28 @@ const Login = () => {
 								<span className="auth__label">E-mail</span>
 								<input
 									{...register("email")}
-									className="input"
+									className={emailClass}
 									type="email"
 								/>
 							</label>
+							<span className="subtext subtext_error">{errors.email?.message}</span>
 						</div>
 						<div className="row mb-16">
 							<label className="flex flex-col wide">
 								<span className="auth__label">Пароль</span>
 								<input
 									{...register("password")}
-									className="input"
+									className={passClass}
 									type="password"
 								/>
 							</label>
+							<span className="subtext subtext_error">{errors.password?.message}</span>
 						</div>
 						<div className="row">
-							<button className="btn btn-primary wide">Войти</button>
+							<button 
+								className="btn btn-primary wide"
+								{...({disabled: btnFlag})}
+							>Войти</button>
 						</div>
 					</form>
 				</div>
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -3,15 +3,33 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { setRegistration } from "../../state/registration/actions";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+	name: yup.string().required("Введите имя"),
+	email: yup.string().email('Неправильный формат').required("Введите email"),
+	password: yup.string().required("Введите пароль")
+});
 
 const Registration = () => {
 	const dispatch = useDispatch();
-	const { register, handleSubmit } = useForm();
+	const { register, watch, handleSubmit, formState: {errors} } = useForm({
+		defaultValues: {name: "", email: "", password: ""},
+		resolver: yupResolver(schema)
+	});
 
 	const onSubmit = (data) => {
 		dispatch(setRegistration(true));
 		alert(JSON.stringify(data));
 	}
+
+	let nameClass = `input ${errors.name ? "input_error" : ""}`;
+	let emailClass = `input ${errors.email ? "input_error" : ""}`;
+	let passClass = `input ${errors.password ? "input_error" : ""}`;
+
+	let values = watch();
+    let btnFlag = (values.email.length > 0 || values.password.length) > 0 ? false : true;
 
 	return (
 		<div className="screen-light">
@@ -23,33 +41,39 @@ const Registration = () => {
 								<span className="auth__label">Имя</span>
 								<input
 									{...register("name")}
-									className="input"
+									className={nameClass}
 									type="text"
 								/>
 							</label>
+							<span className="subtext subtext_error">{errors.name?.message}</span>
 						</div>
 						<div className="row mb-16">
 							<label className="flex flex-col wide">
 								<span className="auth__label">E-mail</span>
 								<input
 									{...register("email")}
-									className="input"
+									className={emailClass}
 									type="email"
 								/>
 							</label>
+							<span className="subtext subtext_error">{errors.email?.message}</span>
 						</div>
 						<div className="row mb-16">
 							<label className="flex flex-col wide">
 								<span className="auth__label">Пароль</span>
 								<input
 									{...register("password")}
-									className="input"
+									className={passClass}
 									type="password"
 								/>
 							</label>
+							<span className="subtext subtext_error">{errors.password?.message}</span>
 						</div>
 						<div className="row">
-							<button className="btn btn-primary wide">Зарегистрироваться</button>
+							<button 
+								className="btn btn-primary wide"
+								{...({disabled: btnFlag})}
+							>Зарегистрироваться</button>
 						</div>
 					</form>
 				</div>
